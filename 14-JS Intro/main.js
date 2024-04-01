d576c685041a7ca38e7f8456865f7a069441cbc2
huseynovelmir <huseynovelmir789@gmail.com>
2024-04-01 20:17:01 +0400

    done

---
+++ 14-JS Intro/main.js
@@ -0,0 +1,58 @@
+//! task1
+// let a = 23;
+// console.log(parseInt(a / 10), "", a % 10);
+
+//! task2
+// let a = 198;
+// console.log(parseInt(a / 100), a % 10);
+
+//! task3
+// let student = 39;
+// let apples = 1200;
+// console.log(apples % student);
+
+//! task4
+// let a = 3;
+// let b = 4;
+// console.log((a+b)*2);
+
+//! task5
+// let a = 65536;
+// let b = 216;
+// console.log(a * b);
+
+//! task6
+// let a = 2;
+// let b = 10;
+// console.log(Math.pow(a, b));
+
+//! task7
+// let a = 512;
+// console.log(parseInt(a/10));
+
+//! task8
+// let a = 27;
+// console.log(parseInt(a%10));
+
+//! task9
+// let a = 27;
+// console.log(parseInt(a%10));
+
+//! task10
+//* Ən zəhləm gedən task
+// let a = 2734;
+// let result = a.toString().split("").reverse().join("");
+// console.log(result*1);
+
+//! task11
+// let cake = 41;
+// let cent = 10;
+// console.log(parseInt(cake/cent));
+
+//! task12
+// let a = 3;
+// console.log("Səthin sahəsi:", 6 * a * a, "Həcmi:", a * a * a);
+
+//! task13
+// let a = 423;
+// console.log(parseInt(a % 10)*100+parseInt(a / 10));
