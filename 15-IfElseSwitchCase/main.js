9dcf046903f52d41710e43afde2e5ea05791eb00
huseynovelmir <huseynovelmir789@gmail.com>
2024-04-04 16:31:04 +0400

    done

---
+++ 15-IfElseSwitchCase/main.js
@@ -0,0 +1,98 @@
+//! Task1
+// let age = 600;
+// let retirementAge = 35;
+
+// if (age > retirementAge && age < 125 && age > 0) {
+//   console.log("Pensiya yaşına düşürsünüz, xeyirli olsun.");
+// } else if (age < retirementAge && age < 125 && age > 0) {
+//   console.log("Pensiya yaşına düşmürsünüz.");
+// } else {
+//   console.log("Düzgün rəqəm daxil edilməyib");
+// }
+
+//! Task2
+// let num = 11;
+// if (num % 2 == 0 && num > 0) {
+//   console.log("Ədəd cütdür");
+// } else if (num % 2 != 0 && num > 0) {
+//     console.log("Ədəd tək ədəddir");
+// }
+// else {
+//     console.log("Ədəd 0 dan kiçikdir.");
+// }
+
+//! Task3
+// let moon = "September";
+// if (
+//   moon == "march" ||
+//   moon == "april" ||
+//   moon == "may" ||
+//   moon == "March" ||
+//   moon == "April" ||
+//   moon == "May"
+// ) {
+//   console.log("Yenə gəldi yaz ayları");
+// } else if (
+//   moon == "june" ||
+//   moon == "july" ||
+//   moon == "august" ||
+//   moon == "June" ||
+//   moon == "July" ||
+//   moon == "August"
+// ) {
+//   console.log("Yay");
+// } else if (
+//   moon == "september" ||
+//   moon == "october" ||
+//   moon == "november" ||
+//   moon == "September" ||
+//   moon == "October" ||
+//   moon == "November"
+// ) {
+//   console.log("Payız");
+// } else {
+//   console.log("Qış");
+// }
+//! Task4
+// let num = 15;
+// if (num % 3 == 0 && num % 5 == 0) {
+//   console.log("Bölünür");
+// } else {
+//   console.log("Bölünmür");
+// }
+
+//! Task5
+// let num = 18;
+// if (num % 2 == 0) {
+//   console.log("Bölünür");
+// } else {
+//   console.log(num % 2);
+// }
+
+//! Task6
+// let a = 18;
+// let b = 1;
+// let count = a * b;
+// if (count > 0) {
+//   console.log("Hasil müsbət ədəddir.");
+// } else if (count < 0) {
+//   console.log("Hasil mənfi ədəddir");
+// } else {
+//   console.log("Hasil 0-a bərabərdir.");
+// }
+
+//! Task7
+// let a = 3;
+// if (1 <= a && a <= 100) {
+//   console.log("ədəd:", a);
+// } else {
+//   console.log("Ədəd 1 və 100 arasında yerləşmir.");
+// }
+
+//! Task8
+// let a = 3;
+// if (1 <= a && a <= 100) {
+//   console.log("ədəd:", a);
+// } else {
+//   console.log("Ədəd 1 və 100 arasında yerləşmir.");
+// }
---
+++ 17-Arrays /app.js
@@ -0,0 +1,5 @@
+let num = 12345;
+let str = String(num);
+for (let i = 0; i < str.length; i++) {
+  console.log(str[0] + str.length - 1);
+}
