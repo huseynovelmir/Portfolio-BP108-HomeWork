a6a2bbe3917f3f27ec8d351c1d96844c084f411e
huseynovelmir <huseynovelmir789@gmail.com>
2024-05-07 23:26:45 +0400

    done

--- 34-AdminPanelUsersProducts/src/components/Checkbox.jsx
+++ 34-AdminPanelUsersProducts/src/components/Checkbox.jsx
@@ -2,35 +2,9 @@ import React from "react";
 
 const Checkbox = ({ products, setProducts }) => {
   return (
-    // <form class="max-w-sm mx-auto p-4">
-    //   <select
-    //     id="countries"
-    //     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-    //   >
-    //     <option selected>Choose a sort method (Default)</option>
-    //     <option
-    //       value="name-az"
-    //       onSelect={() => {
-    //         let newarr = [...products].sort((a, b) =>
-    //           a.title[0].localeCompare(b.title[0])
-    //         );
-    //         setProducts(newarr);
-    //       }}
-    //     >
-    //       Name: a-z
-    //     </option>
-    //     <option value="name-za">Name: z-a</option>
-    //     <option value="pricemaxmin">Price: max-min</option>
-    //     <option value="priceminmax">Price: min-max</option>
-    //     <option value="ratingmaxmin">Rating: max-min</option>
-    //     <option value="ratingminmax">Rating: min-max</option>
-    //     <option value="countmaxmin">Count: max-min</option>
-    //     <option value="countminmax">Count: min-max</option>
-    //   </select>
-    // </form>
     <div className="w-100 flex align-items-center justify-center gap-10 p-5">
       <button
-        className=" bg-white p-2 rounded-md bg-cyan-400"
+        className="p-2 rounded-md bg-blue-500"
         onClick={() => {
           let newarr = [...products].sort((a, b) =>
             a.title[0].localeCompare(b.title[0])
@@ -41,7 +15,7 @@ const Checkbox = ({ products, setProducts }) => {
         Name: a-z
       </button>
       <button
-        className=" bg-white p-2 rounded-md bg-cyan-400"
+        className="p-2 rounded-md bg-blue-500"
         onClick={() => {
           let newarr = [...products].sort((a, b) =>
             b.title[0].localeCompare(a.title[0])
@@ -52,7 +26,7 @@ const Checkbox = ({ products, setProducts }) => {
         Name: z-a
       </button>
       <button
-        className=" bg-white p-2 rounded-md bg-cyan-400"
+        className="p-2 rounded-md bg-blue-500"
         onClick={() => {
           let newarr = [...products].sort((a, b) => b.price - a.price);
           setProducts(newarr);
@@ -62,7 +36,7 @@ const Checkbox = ({ products, setProducts }) => {
       </button>
 
       <button
-        className=" bg-white p-2 rounded-md bg-cyan-400"
+        className="p-2 rounded-md bg-blue-500"
         onClick={() => {
           let newarr = [...products].sort((a, b) => a.price - b.price);
           setProducts(newarr);
@@ -71,7 +45,7 @@ const Checkbox = ({ products, setProducts }) => {
         Price: min-max
       </button>
       <button
-        className=" bg-white p-2 rounded-md bg-cyan-400"
+        className="p-2 rounded-md bg-blue-500"
         onClick={() => {
           let newarr = [...products].sort(
             (a, b) => b.rating.rate - a.rating.rate
@@ -82,7 +56,7 @@ const Checkbox = ({ products, setProducts }) => {
         Rate: max-min
       </button>
       <button
-        className=" bg-white p-2 rounded-md bg-cyan-400"
+        className="p-2 rounded-md bg-blue-500"
         onClick={() => {
           let newarr = [...products].sort(
             (a, b) => a.rating.rate - b.rating.rate
@@ -93,7 +67,7 @@ const Checkbox = ({ products, setProducts }) => {
         Rate: min-max
       </button>
       <button
-        className=" bg-white p-2 rounded-md bg-cyan-400"
+        className="p-2 rounded-md bg-blue-500"
         onClick={() => {
           let newarr = [...products].sort(
             (a, b) => b.rating.count - a.rating.count
@@ -104,7 +78,7 @@ const Checkbox = ({ products, setProducts }) => {
         Count: max-min
       </button>
       <button
-        className=" bg-white p-2 rounded-md bg-cyan-400"
+        className="p-2 rounded-md bg-blue-500"
         onClick={() => {
           let newarr = [...products].sort(
             (a, b) => a.rating.count - b.rating.count
--- 34-AdminPanelUsersProducts/src/components/Table.jsx
+++ 34-AdminPanelUsersProducts/src/components/Table.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Checkbox from "./Checkbox";
+import SearchBar from "./SearchBar";
 const Table = () => {
   const [products, setProducts] = useState([]);
   const data = async () => {
@@ -12,6 +13,7 @@ const Table = () => {
   }, []);
   return (
     <div className="relative overflow-x-auto">
+      <SearchBar products={products} setProducts={setProducts} />
       <Checkbox products={products} setProducts={setProducts} />
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -52,7 +54,15 @@ const Table = () => {
                 <img src={el.image} alt="" className="w-20 h-20" />
               </td>
               <td className="px-6 py-4">{el?.rating?.rate}</td>
-              <td className="px-6 py-4">{el?.rating?.count}</td>
+              <td
+                className={
+                  el.rating.count < 100
+                    ? "px-6 py-4 text-red-400"
+                    : "px-6 py-4 text-yellow-400"
+                }
+              >
+                {el?.rating?.count}
+              </td>
               <td className="px-6 py-4">{el.price}$</td>
             </tr>
           ))}
