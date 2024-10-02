fd631ad22a8c950693b18a3311787a61a8ab7683
Elmir <huseynovelmir789@gmail.com>
2024-10-02 21:24:14 +0400

    Update array iteration methods

--- 20-ArrayİterationMethods/app.js
+++ 20-ArrayİterationMethods/app.js
@@ -253,17 +253,202 @@ let students = [
     salaryAZN: 100,
   },
 ];
-// !- Bütün elementləri konsola yazın
-// students.forEach((element) => console.log(element));
-//! - Bütün tələbələrin adlarını konsola yazın - ad: Qurban
-// students.forEach((element) => console.log(element.name));
-//! - Bütün tələbələrin adlarını və soyadlarını konsola yazın - ad: Qurban, soyad: Qurbanzada
-// students.forEach((element) =>
-//   console.log("Ad:", element.name, "Surname:", element.surname)
-// );
+// !Task 1
+// console.log(students);
+// !Task 2
+// students.map((item) => console.log(item.name));
+// !Task 3
+// students.map((item) => console.log("Name:",item.name,"Surname:",item.surname));
+// !Task 4
+// students.map((item) => {
+//   if (item.fail == true) {
+//     console.log("Name:", item.name, "Surname:", item.surname);
+//   }
+// });
+// !Task 5
+// let student = students[0];
+// for (let i = 0; i < students.length; i++) {
+//   if (students[i].hobbies.length > student.hobbies.length) {
+//     student = students[i];
+//   }
+// }
+// console.log("Adı:", student.name, "hobbiləri:", student.hobbies.join(","));
+// !Task 6
+// let student = students[0];
+// for (let i = 0; i < students.length; i++) {
+//   if (students[i].avgPoint > student.avgPoint) {
+//     student = students[i];
+//   }
+// }
 
+// console.log("Adı:", student.name, "Soyadı:", student.surname);
+//  !Task 7
+// let student = students[0];
+// for (let i = 0; i < students.length; i++) {
+//   if (
+//     students[i].loginDetail.password.length >
+//     student.loginDetail.password.length
+//   ) {
+//     student = students[i];
+//   }
+// }
+// console.log(student.name);
 
-/*
+// !Task 8
+// students.map((student) => {
+//   if (student.boyfriendGirlfriend == true) {
+//     console.log(student.name);
+//   }
+// });
+// !Task 9
+// students.map((student) => {
+//   if (student.age == 21) {
+//     console.log(
+//       "Name:",
+//       student.name,
+//       ",Teacher Name:",
+//       student.teacher.map((teacher) => teacher.name).join(",")
+//     );
+//   }
+// });
+// !Task 10
+// students.map((student) => {
+//   let result = student.salaryAZN * 1.7;
+//   console.log(Math.round(result), "$");
+// });
+// !Task 11
+// students.forEach((student) => {
+//   student.teacher.forEach((teacher) => {
+//     console.log(
+//       teacher.name.charAt(0).toUpperCase() + teacher.name.slice(1).toLowerCase()
+//     );
+//   });
+// });
+
+// !Task 12
+// students.forEach((student) => {
+//   if (!/\d/.test(student.loginDetail.password)) {
+//     console.log(student.name);
+//   }
+// });
+// !Task 13
+// students.forEach((student) => {
+//   if (student.gender == "girl") {
+//     student.teacher.forEach((teacher) => {
+//       if (teacher.id == 3) {
+//         console.log(student.name);
+//       }
+//     });
+//   }
+// });
+// ! Task 14
+// students.forEach((student) => {
+//   console.log(student.name, student.teacher.length);
+// });
+
+// ! Task 15
+// let arr = students.map((student) => ({
+//   name: student.name,
+//   username: student.loginDetail.username,
+//   password: student.loginDetail.password,
+// }));
+
+// console.log(arr)
+// !Task 16
+// students.map((student) => {
+//   student.teacher.map((teacher) => {
+//     console.log(teacher.name.concat(" Müəllim"));
+//   });
+// });
+// ! Task 17
+// students.forEach((student) => {
+//   let newPassword = student.loginDetail.password.padStart(
+//     student.loginDetail.password.length + 3,
+//     " "
+//   );
+//   console.log(newPassword);
+// });
+// !Task 18
+// students.forEach((student) => {
+//   let newPassword = student.loginDetail.password;
+//   const visiblePart = newPassword.substring(0, 4);
+//   const hiddenPart = "*".repeat(newPassword.length - 4);
+//   let result = visiblePart + hiddenPart;
+//   console.log(result);
+// });
+
+// !Task 19
+// students.forEach((student) => {
+//   let studentSurname = student.surname.slice(0, student.name.length);
+//   console.log(student.name, studentSurname);
+// });
+// !TAsk 20
+// let result = 0;
+// students.forEach((student) => {
+//   result += student.age;
+// });
+// console.log(result);
+
+// !Task 21
+// let result = 0;
+// students.forEach((student) => {
+//   result += student.avgPoint;
+// });
+// console.log(Math.ceil(result / students.length));
+//  !Task 22
+// students.map((student) => {
+//   if (student.hobbies.includes("book")) {
+//     console.log(student.name);
+//   }
+// });
+// !Task 23
+// let result = 0;
+// students.map((student) => {
+//   result += student.age;
+// });
+// console.log(Math.ceil(result / students.length));
+// !Task 24
+
+// let arr = [];
+// students.map((student) => {
+//   let obj = {
+//     name: student.name,
+//     age: student.age,
+//   };
+//   arr.push(obj);
+// });
+// console.log(arr);
+
+// !Task 25
+// let nameLength = 0;
+// let surnameLength = 0;
+// students.map((student) => {
+//   nameLength += student.name.length;
+//   surnameLength += student.surname.length;
+// });
+// console.log(nameLength + surnameLength);
+// !Task 26
+
+// students.map((student) => {
+//   console.log(student.name.split("").reverse().join(""));
+// });
+
+// !Task 27
+// let studentArr = [];
+// students.map((student) => {
+//   if (student.hobbies.includes("gaming")) {
+//     studentArr.push(student.name);
+//   }
+// });
+// console.log(studentArr);
+
+// !Task 28
+// let arr = [];
+// students.map((student) => {
+//   arr.push(student.hobbies);
+// });
+// console.log(arr);
+/* 
 ███████╗██████╗░██╗░░░██╗███████╗░█████╗░███████╗
 ██╔════╝██╔══██╗██║░░░██║██╔════╝██╔══██╗██╔════╝
 █████╗░░██║░░██║██║░░░██║██████╗░██║░░██║█████╗░░
---
+++ 20-ArrayİterationMethods/readme.md
@@ -0,0 +1,55 @@
+#### 1.Bütün elementləri konsola yazın
+
+#### 2.Bütün tələbələrin adlarını konsola yazın - ad: Qurban
+
+#### 3.Bütün tələbələrin adlarını və soyadlarını konsola yazın ad: Qurban, soyad: Qurbanzada
+
+#### 4.Kəsri olan tələbələrin ad və soyadını çapa verin
+
+#### 5.Ən çox hobbisi olan tələbənin ad və hobbilərini çapa verin
+
+#### 6.Ortalaması ən böyük olan tələbənin ad və soyadını çapa verin
+
+#### 7.Parolu ən uzun olan tələbənin username və adını çapa verin
+
+#### 8.boyfriend-i olan tələbələrin adlarını və username-lərini çapa verin
+
+#### 9.Yaşı 20 olan tələbələrin adlarını və müəllim adlarını çapa verin
+
+#### 10.Bütün tələbələrə təqaüdlərinin dollar ilə görünməsini təmin edin
+
+#### 11.Müəllimlərin baş hərflərini böyük hərflə yazın
+
+#### 12.Şifrəsində rəqəm olmayan tələbələri tapın
+
+#### 13.Qızların arasında id-si 3 olan müəllimlərin adını deyin
+
+#### 14.Tələbələrin adlarını və müəllim saylarını çapa verin
+
+#### 15.Tələbələrin adlarını username və şifrələrindən ibarət yeni array yaradın [{name:qurban,username:qurban123,password:qqq123}]
+
+#### 16. Müəllimlərin adlarından sonra müəllim sözünü əlavə edin. String metod istifadə edin
+
+#### 17.Hamının şifrəsinin əvvəlinə 3 ədəd boşluq əlavə edin. String metod istifadə edin
+
+#### 18.Hamının şifrəsinin ilk 4 simvolu görsənsin yerdə qalanlar "\*" ilə əvəz olunsun. String metod istifadə edin
+
+#### 19.Hamının soyadının uzunluğu adının uzunluğu qədər olsun. String metod istifadə edin
+
+#### 20.Bütün tələbələrin yaşlarının toplamı nə qədərdir
+
+#### 21.Bütün tələbələrin cəmi bal ortalaması nə qədərdir
+
+#### 22."book" həvəsini olan tələbələrin siyahısını çap edin
+
+#### 23.Tələbələrin yaş ortalaması nədir?
+
+#### 24.Hər bir tələbə üçün cinsiyət və yaşı göstərən bir obyekt yaradin
+
+#### 25.Bütün tələbələrin ad və soyadlarının uzunluqlarının cəmi hansıdır
+
+#### 26.Tələbələrin adlarını tərsinə çevirin.
+
+#### 27."gamig" həvəsi olan tələbələrin array-ini yaradın
+
+#### 28.Bütün tələbələrin hobbiləri olan array yaradın?
